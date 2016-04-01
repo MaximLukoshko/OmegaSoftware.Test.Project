b5bc262fbbe4a029bb6bc50479cebb93fa99a132
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-04-01 17:40:14 +0300

    FirstCommit
    First Commit

---
+++ OmegaSoftware.Test.Project/OmegaSoftware.Test.Project.h
@@ -0,0 +1,40 @@
+
+// OmegaSoftware.Test.Project.h : главный файл заголовка для приложения OmegaSoftware.Test.Project
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"       // основные символы
+
+
+// COmegaSoftwareTestProjectApp:
+// О реализации данного класса см. OmegaSoftware.Test.Project.cpp
+//
+
+class COmegaSoftwareTestProjectApp : public CWinAppEx
+{
+public:
+	COmegaSoftwareTestProjectApp();
+
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Реализация
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern COmegaSoftwareTestProjectApp theApp;
